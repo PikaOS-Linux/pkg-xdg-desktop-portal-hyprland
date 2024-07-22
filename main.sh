215a3af140971f092d4f8ef9f337782cd0e6e70e
ferrreo <harderthanfire@gmail.com>
2024-07-22 12:43:06 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -5,7 +5,7 @@ DEBIAN_FRONTEND=noninteractive
 apt-get update
 
 # Clone Upstream
-git clone --depth=1 --recurse-submodules https://github.com/hyprwm/xdg-desktop-portal-hyprland.git -b v1.3.2
+git clone --depth=1 --recurse-submodules https://github.com/hyprwm/xdg-desktop-portal-hyprland.git -b v1.3.3
 cd xdg-desktop-portal-hyprland
 cp -rvf ../debian ./
 
